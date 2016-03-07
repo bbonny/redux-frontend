49932e626c86e419b56f8dd3b6054e456495f886
Benjamin Bonny <benjamin.bonny@gmail.com>
2016-03-07 20:07:27 +0100

    Remove a lot of stuff

--- src/routes.js
+++ src/routes.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import { isLoaded as isAuthLoaded, load as loadAuth } from 'redux/modules/auth';
 import {
     App,
     Home,
-    About,
     NotFound,
   } from 'containers';
 
-export default (store) => {
-  const requireLogin = (nextState, replace, cb) => {
-    function checkAuth() {
-      const { auth: { user }} = store.getState();
-      if (!user) {
-        // oops, not logged in, so can't be here!
-        replace('/');
-      }
-      cb();
-    }
-
-    if (!isAuthLoaded(store.getState())) {
-      store.dispatch(loadAuth()).then(checkAuth);
-    } else {
-      checkAuth();
-    }
-  };
-
+export default () => {
   /**
    * Please keep routes in alphabetical order
    */
@@ -33,9 +14,6 @@ export default (store) => {
     <Route path="/" component={App}>
       { /* Home (main) route */ }
       <IndexRoute component={Home}/>
-
-      { /* Routes */ }
-      <Route path="about" component={About}/>
 
       { /* Catch all route */ }
       <Route path="*" component={NotFound} status={404} />

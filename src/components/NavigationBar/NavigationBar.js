131c6d3aa0dbab33511c4b875d9c7d04a0e83795
laertness <laertbejtaj@gmail.com>
2016-03-19 13:49:35 +0100

    move nav bar to a new component

---
+++ src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import { IndexLink } from 'react-router';
+import { LinkContainer } from 'react-router-bootstrap';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import Nav from 'react-bootstrap/lib/Nav';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+
+import config from '../../config';
+
+export default class NavigationBar extends Component {
+    render() {
+      return (
+        <div>
+          <Navbar fixedTop>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <IndexLink to="/" activeStyle={{color: '#ffffff'}}>
+                  <span>{config.app.title}</span>
+                </IndexLink>
+              </Navbar.Brand>
+            </Navbar.Header>
+            <Nav pullRight>
+              <NavDropdown title="Mode" id="nav-dropdown">
+                <LinkContainer to="/">
+                  <MenuItem>User Mode</MenuItem>
+                </LinkContainer>
+                <LinkContainer to="/admin">
+                  <MenuItem>Admin Mode</MenuItem>
+                </LinkContainer>
+              </NavDropdown>
+            </Nav>
+          </Navbar>
+        </div>
+     );
+    }
+}

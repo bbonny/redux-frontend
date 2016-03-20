19dbabe439754ae660be2d379290b40ed8313f17
Benjamin Bonny <benjamin.bonny@gmail.com>
2016-03-20 15:38:43 +0100

    fix tests

--- src/components/__tests__/BricksColumn-test.js
+++ src/components/__tests__/BricksColumn-test.js
@@ -20,10 +20,11 @@ describe('BricksColumn', () => {
       checked: {value: true},
       name: {value: "Banana"},
     },
-  ]
+  ];
+  const name = "I am a column";
 
   const renderer = renderIntoDocument(
-    <BricksColumn bricks={bricks} />
+    <BricksColumn bricks={bricks} name={name}/>
   );
   const dom = ReactDOM.findDOMNode(renderer);
 
@@ -31,18 +32,23 @@ describe('BricksColumn', () => {
     return expect(renderer).to.be.ok;
   });
 
+  it('should render with correct name', () => {
+    const text = dom.getElementsByTagName('h5')[0].textContent;
+    expect(text).to.equal(name);
+  });
+
   it('should render with correct value (first brick)', () => {
-    const text = dom.getElementsByTagName('label')[0].textContent;
+    const text = dom.querySelectorAll('.row > div')[0].textContent;
     expect(text).to.equal(bricks[0].name.value);
   });
 
   it('should render with correct value (second brick)', () => {
-    const text = dom.getElementsByTagName('label')[1].textContent;
+    const text = dom.querySelectorAll('.row')[1].querySelectorAll('div')[0].textContent;
     expect(text).to.equal(bricks[1].name.value);
   });
 
   it('should have the correct number of bricks', () => {
-    const labels = dom.getElementsByTagName('label');
+    const labels = dom.querySelectorAll('.row');
     expect(labels.length).to.equal(bricks.length);
   });
 });

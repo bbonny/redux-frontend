6096934db6cdeea5936635812d3e2ed5d69ace1a
laertness <laertbejtaj@gmail.com>
2016-03-21 23:09:03 +0100

    form looks better

--- src/components/PresentationConfigurator/PresentationConfigurator.js
+++ src/components/PresentationConfigurator/PresentationConfigurator.js
@@ -1,6 +1,6 @@
 import React, {Component, PropTypes} from 'react';
-import {PureInput} from 'components';
-import {Grid} from 'react-bootstrap/lib/Grid';
+import {Col, Grid, Input, Panel, Row} from 'react-bootstrap/lib';
+
 
 export default class PresentationConfigurator extends Component {
   static propTypes = {
@@ -13,13 +13,34 @@ export default class PresentationConfigurator extends Component {
     } = this.props;
 
     return (
-      <div className="form-group">
-<Grid componentClass="div"/>
-      <PureInput type="text" className="form-control" placeholder="Client Name" field={clientName}/><br/>
-        <PureInput type="text" className="form-control" placeholder="Sales Name" field={salesName}/><br/>
-        <PureInput type="text" className="form-control" placeholder="Template Name" field={templateName}/><br/>
-        <PureInput type="text" className="form-control" placeholder="Dividers" field={dividers}/><br/>
-      </div>
+      <Panel>
+        <Grid>
+          <Row>
+            <Col xs={6} xsOffset={3} md={4} mdOffset={1}>
+              <Input type="text" label="Client Name" placeholder="Enter client name" {...clientName}/>
+            </Col>
+            <Col xs={6} xsOffset={3} md={4} mdOffset={1}>
+              <Input type="text" label="Sales Name" placeholder="Enter your name" {...salesName}/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} xsOffset={3} md={4} mdOffset={1}>
+              <Input type="select" label="Template Name" placeholder="Template Name" {...templateName}>
+                <option value="0">Classic</option>
+              </Input>
+            </Col>
+            <Col xs={6} xsOffset={3} md={4} mdOffset={1}>
+              <Input type="select" label="Dividers" placeholder="Dividers" {...dividers}>
+                <option value="0">Without dividers</option>
+                <option value="1">One divider</option>
+                <option value="2">Two dividers</option>
+                <option value="3">Three dividers</option>
+                <option value="4">Four dividers</option>
+              </Input>
+            </Col>
+          </Row>
+        </Grid>
+      </Panel>
     );
   }
 }

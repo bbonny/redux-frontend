e83b8dcb16d2ad89fa9ce52d834f787e4152d3c9
Benjamin Bonny <benjamin.bonny@gmail.com>
2016-03-23 21:41:50 +0100

    Put labels on the same line than inputs in presconfigurator

--- src/components/PresentationConfigurator/PresentationConfigurator.js
+++ src/components/PresentationConfigurator/PresentationConfigurator.js
@@ -15,30 +15,53 @@ export default class PresentationConfigurator extends Component {
 
     return (
       <div className={styles.configurator}>
-          <Row>
-            <Col xs={12} md={6}>
-              <Input type="text" label="Client Name" placeholder="Enter client name" {...clientName}/>
-            </Col>
-            <Col xs={12} md={6}>
-              <Input type="text" label="Sales Name" placeholder="Enter your name" {...salesName}/>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs={12} md={6}>
-              <Input type="select" label="Template Name" placeholder="Template Name" {...templateName}>
-                <option value="0">Classic</option>
-              </Input>
-            </Col>
-            <Col xs={12} md={6}>
-              <Input type="select" label="Dividers" placeholder="Dividers" {...dividers}>
-                <option value="0">Without dividers</option>
-                <option value="1">One divider</option>
-                <option value="2">Two dividers</option>
-                <option value="3">Three dividers</option>
-                <option value="4">Four dividers</option>
-              </Input>
-            </Col>
-          </Row>
+        <Row>
+          <Col xs={12} sm={6}>
+            <Input
+              labelClassName="col-xs-4 col-sm-4"
+              wrapperClassName="col-xs-8 col-sm-8"
+              type="text"
+              label="Client Name"
+              placeholder="Enter client name"
+              {...clientName}
+            />
+          </Col>
+          <Col xs={12} sm={6}>
+            <Input
+              labelClassName="col-xs-4"
+              wrapperClassName="col-xs-8"
+              type="text"
+              label="Sales Name"
+              placeholder="Enter your name"
+              {...salesName}
+            />
+          </Col>
+          <Col xs={12} sm={6}>
+            <Input
+              labelClassName="col-xs-4 col-sm-4"
+              wrapperClassName="col-xs-8 col-sm-8"
+              type="select"
+              label="Template Name"
+              placeholder="Template Name"
+              {...templateName}
+            >
+              <option value="0">Classic</option>
+            </Input>
+          </Col>
+          <Col xs={12} sm={6}>
+            <Input
+              labelClassName="col-xs-4 col-sm-4"
+              wrapperClassName="col-xs-8 col-sm-8"
+              type="select"
+              label="Dividers"
+              placeholder="Dividers"
+              {...dividers}
+            >
+              <option value="0">Yes</option>
+              <option value="1">No</option>
+            </Input>
+          </Col>
+        </Row>
       </div>
     );
   }
